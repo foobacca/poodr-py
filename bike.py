0481818ce2723dd78b646c7e744c31a1b97eff87
Hamish Downer <hamish@aptivate.org>
2014-12-31 10:41:22 +0000

    The Wheel Class completed (p34)

--- bike.py
+++ bike.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import math
 
 
 class Wheel(object):
@@ -10,6 +11,9 @@ class Wheel(object):
     def diameter(self):
         return self.rim + (self.tyre * 2)
 
+    def circumference(self):
+        return self.diameter() * math.pi
+
 
 def wheelify(data):
     return [Wheel(cell[0], cell[1]) for cell in data]
@@ -17,10 +21,10 @@ def wheelify(data):
 
 class Gear(object):
 
-    def __init__(self, chainring, cog, rim, tyre):
+    def __init__(self, chainring, cog, wheel=None):
         self.chainring = chainring
         self.cog = cog
-        self.wheel = Wheel(rim, tyre)
+        self.wheel = wheel
 
     def ratio(self):
         return self.chainring / float(self.cog)
@@ -35,14 +39,17 @@ class RevealingReferences(object):
         self.wheels = wheelify(data)
 
     def diameters(self):
-        return [self.diameter(wheel) for wheel in self.wheels]
-
-    def diameter(self, wheel):
-        return wheel.rim + (wheel.tyre * 2)
+        return [wheel.diameter() for wheel in self.wheels]
 
 
-print Gear(52, 11, 26, 1.5).gear_inches()
-print Gear(52, 11, 24, 1.25).gear_inches()
+print Gear(52, 11, Wheel(26, 1.5)).gear_inches()
+print Gear(52, 11, Wheel(24, 1.25)).gear_inches()
 
 data = [[622, 20], [622, 23], [559, 30], [559, 40]]
 print RevealingReferences(data).diameters()
+
+wheel = Wheel(26, 1.5)
+print wheel.circumference()
+
+print Gear(52, 11, wheel).gear_inches()
+print Gear(52, 11).ratio()
