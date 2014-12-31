037a44d808ddcd37a30e6844dafebbec9a134415
Hamish Downer <hamish@aptivate.org>
2014-12-31 10:20:46 +0000

    add gear_inches() (p20)

--- bike.py
+++ bike.py
@@ -3,13 +3,18 @@
 
 class Gear(object):
 
-    def __init__(self, chainring, cog):
+    def __init__(self, chainring, cog, rim, tyre):
         self.chainring = chainring
         self.cog = cog
+        self.rim = rim
+        self.tyre = tyre
 
     def ratio(self):
         return self.chainring / (self.cog * 1.0)
 
+    def gear_inches(self):
+        return self.ratio() * (self.rim + (self.tyre * 2))
 
-print Gear(52, 11).ratio()
-print Gear(30, 27).ratio()
+
+print Gear(52, 11, 26, 1.5).gear_inches()
+print Gear(52, 11, 24, 1.25).gear_inches()
