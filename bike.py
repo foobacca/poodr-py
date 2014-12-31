a3c49696ec32c8252bf0df71f1f3e5fa7d38af1c
Hamish Downer <hamish@aptivate.org>
2014-12-31 10:29:43 +0000

    Change ObscuringReferences to RevealingReferences (p28)
    and along the way create the Wheel class and the wheelify method

--- bike.py
+++ bike.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python
 
 
+class Wheel(object):
+
+    def __init__(self, rim, tyre):
+        self.rim = rim
+        self.tyre = tyre
+
+
+def wheelify(data):
+    return [Wheel(cell[0], cell[1]) for cell in data]
+
+
 class Gear(object):
 
     def __init__(self, chainring, cog, rim, tyre):
@@ -16,17 +27,17 @@ class Gear(object):
         return self.ratio() * (self.rim + (self.tyre * 2))
 
 
+class RevealingReferences(object):
+
+    def __init__(self, data):
+        self.wheels = wheelify(data)
+
+    def diameters(self):
+        return [wheel.rim + (wheel.tyre * 2) for wheel in self.wheels]
+
+
 print Gear(52, 11, 26, 1.5).gear_inches()
 print Gear(52, 11, 24, 1.25).gear_inches()
 
-
-class ObscuringReferences(object):
-
-    def __init__(self, data):
-        self.data = data
-
-    def diameters(self):
-        return [cell[0] + (cell[1] * 2) for cell in self.data]
-
 data = [[622, 20], [622, 23], [559, 30], [559, 40]]
-print ObscuringReferences(data).diameters()
+print RevealingReferences(data).diameters()
