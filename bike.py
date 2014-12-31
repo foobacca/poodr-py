025b2028072ce45b30e95fbfe1b260ecd39203ef
Hamish Downer <hamish@aptivate.org>
2014-12-31 10:25:22 +0000

    add ObscuringReference (p26)

--- bike.py
+++ bike.py
@@ -18,3 +18,15 @@ class Gear(object):
 
 print Gear(52, 11, 26, 1.5).gear_inches()
 print Gear(52, 11, 24, 1.25).gear_inches()
+
+
+class ObscuringReferences(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    def diameters(self):
+        return [cell[0] + (cell[1] * 2) for cell in self.data]
+
+data = [[622, 20], [622, 23], [559, 30], [559, 40]]
+print ObscuringReferences(data).diameters()
