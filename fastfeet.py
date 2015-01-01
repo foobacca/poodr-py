8fdddf8387d994fbd58e5deeae2a0fe3dcd55fa5
Hamish Downer <hamish@aptivate.org>
2015-01-01 10:34:37 +0000

    Change prepare() to use duck typing (p94)

--- fastfeet.py
+++ fastfeet.py
@@ -3,21 +3,20 @@
 
 class Trip(object):
 
+    def __init__(self):
+        self.bicycles = []
+        self.customers = []
+        self.vehicle = None
+
     def prepare(self, preparers):
         for preparer in preparers:
-            if isinstance(preparer, Mechanic):
-                preparer.prepare_bicycles(self.bicycles)
-            elif isinstance(preparer, TripCoordinator):
-                preparer.buy_food(self.customers)
-            elif isinstance(preparer, Driver):
-                preparer.fuel_up(self.vehicle)
-                preparer.fill_water_tank(self.vehicle)
+            preparer.prepare_trip(self)
 
 
 class Mechanic(object):
 
-    def prepare_bicycles(self, bicycles):
-        for bicycle in bicycles:
+    def prepare_trip(self, trip):
+        for bicycle in trip.bicycles:
             self.prepare_bicycle(bicycle)
 
     def prepare_bicycle(self, bicycle):
@@ -26,11 +25,18 @@ class Mechanic(object):
 
 class TripCoordinator(object):
 
+    def prepare_trip(self, trip):
+        self.buy_food(trip.customers)
+
     def buy_food(self, customers):
         pass
 
 
 class Driver(object):
+
+    def prepare_trip(self, trip):
+        self.fuel_up(trip.vehicle)
+        self.fill_water_tank(trip.vehicle)
 
     def fuel_up(self, vehicle):
         pass
